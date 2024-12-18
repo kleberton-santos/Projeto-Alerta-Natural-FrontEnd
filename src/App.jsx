9ed0fdddb489033bd58b2f6aae1fc1273e7564b2
kleberton-santos <ti.klebersantos@gmail.com>
2024-12-17 21:37:47 -0300

    Inicio do componente secao feed | import bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
+import SecaoFeedPublicacao from './components/SecaoFeedPublicacao'
+
 
 
 function App() {
+
+    return (
+        <div>
+            <SecaoFeedPublicacao/>
+        </div>
+    )
 
 }
 
---
+++ src/components/SecaoFeedPublicacao.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../assets/Css/SecaoFeedPublicacao.css'
+
+const SecaoFeedPublicacao = () => {
+    return (
+        <div>
+        <div className="secaoPrincipal">
+            <div className="secaoFoto">
+                {/* Conteúdo da seção de foto */}
+            </div>
+            <div className="secaoPublicacao">
+                {/* Conteúdo da seção de publicação */}
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default SecaoFeedPublicacao;
