4f2df79a37696e1b6e4fd61689fb0d1ec187199f
kleberton-santos <ti.klebersantos@gmail.com>
2024-12-21 16:46:55 -0300

    aplicado fonte de forma global e ajustado o tamanho direto no css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import './index.css';
 //import SecaoFeedPublicacao from './components/feed/SecaoFeedPublicacao.jsx' 
-import SecaoFeedTimeLine from './components/feed/SecaoFeedTimeLine.jsx' 
+//import SecaoFeedTimeLine from './components/feed/SecaoFeedTimeLine.jsx' 
+import SecaoFeedAmigos from './components/feed/SecaoFeedAmigos.jsx' 
 
 function App() {
     return (
         <div>
-            <SecaoFeedTimeLine />
+            <SecaoFeedAmigos />
         </div>
     )
 }
---
+++ src/components/feed/SecaoFeedAmigos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../assets/Css/feed/SecaoFeedAmigos.css"; 
+
+const SecaoFeedAmigos = () => {
+    return (
+        <div className="seca-feed-time-line d-flex flex-column align-items-center rounded bg-custom mt-3 ms-4" style={{ height: '400px', width: '12%', borderStyle: 'solid' }}>
+            <p className="secao-feed-text-amigo text-white mt-2">Amigos</p>
+        </div>
+    )
+}
+
+export default SecaoFeedAmigos;
