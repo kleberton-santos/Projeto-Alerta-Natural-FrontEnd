6bb8eb856ba32ff0bd6217554613b12f321c9199
kleberton-santos <ti.klebersantos@gmail.com>
2024-12-27 18:12:48 -0300

    layout tela de feed

--- src/components/feed/SecaoFeedPublicacao.jsx
+++ src/components/feed/SecaoFeedPublicacao.jsx
@@ -3,12 +3,13 @@ import "../../assets/Css/feed/secaoFeedPublicacao.css";
 
 const SecaoFeedPublicacao = () => {
     return (
-        <div className="secaoPrincipal d-flex rounded bg-custom mt-3 ms-4" style={{ height: '100px', width: '60%', borderStyle: 'solid' }}>
+        <div className="secao-feed-publicacao d-flex rounded bg-custom mt-3 ms-4" style={{ height: '100px', width: '60%', borderStyle: 'solid' }}>
+
             <div className="foto-Perfil rounded-circle bg-secondary ms-3 mt-3" style={{ height: '60px', width: '60px' }}>
                 {/* Conteúdo da foto */}
             </div>
-            <div className="secao-publicacao border-2 m-auto ms-5 me-4 rounded bg-custom-publicacao" style={{ height: '70%', flex: 1, border: '2px solid' }}>
-                {/* Conteúdo da seção de publicação */}
+            <div className="secao-publicacao border-2 m-auto ms-5 me-4 rounded bg-custom-publicacao d-flex justify-content-center align-items-center"  style={{ height: '70%', flex: 1, border: '2px solid' }}>
+                 <p className="texto-central mt-3 text-white">Faça sua Publicação</p>
             </div>
         </div>
     )
