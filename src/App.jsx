f74301c0cf7533490fbdebab3a03031bbfb03bc0
kleberton-santos <ti.klebersantos@gmail.com>
2024-12-29 12:29:42 -0300

    banner header estatico concluido

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './index.css';
 import Feed from './pages/Feed';
+import HeaderGlobal from './components/header/HeaderGlobal';
 
 function App() {
     return (
         <div>
-            <Feed/>
+            <HeaderGlobal/>
         </div>
     )
 }
