3f6132db3aca548cf531880ed72398995bd449a8
kleberton-santos <ti.klebersantos@gmail.com>
2024-12-29 18:48:40 -0300

    Criação das rotas das paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { useState } from 'react'
 import './index.css';
-import Home from './pages/Home';
-import Previsao from './pages/Previsao';
-import BuscaLocalizacao from './pages/BuscaLocalizacao';
-import Feed from './pages/Feed';
-import Contato from './pages/Contato';
-import Login from './pages/Login';
-import Cadastro from './pages/Cadastro';
-import HeaderGlobal from './components/header/HeaderGlobal';
-import FooterGlobal from './components/footer/FooterGlobal';
+
+import AppRoutes from './routes';
 
 function App() {
     return (
         <div>
-            <Cadastro/>
+            <AppRoutes/>
         </div>
     )
 }
---
+++ src/routes.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Previsao from "./pages/Previsao"
+import BuscaLocalizacao from "./pages/BuscaLocalizacao"
+import Feed from "./pages/feed"
+import Contato from "./pages/Contato"
+import Login from "./pages/Login"
+import Cadastro from "./pages/Cadastro"
+
+const AppRoutes = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Home />}></Route>
+                <Route path="/previsao" element={ <Previsao />}></Route>
+                <Route path="/buscaLocalizacao" element={ <BuscaLocalizacao />}></Route>
+                <Route path="/feed" element={ <Feed />}></Route>
+                <Route path="/contato" element={ <Contato />}></Route>
+                <Route path="/login" element={ <Login />}></Route>
+                <Route path="/cadastro" element={ <Cadastro />}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default AppRoutes
