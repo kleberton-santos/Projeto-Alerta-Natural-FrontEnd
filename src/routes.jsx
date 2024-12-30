d980dac86a097704e70b392a161350b7a55b4534
kleberton-santos <ti.klebersantos@gmail.com>
2024-12-29 21:25:40 -0300

    Remoção de tela de busca

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Previsao from "./pages/Previsao"
-import BuscaLocalizacao from "./pages/BuscaLocalizacao"
 import Feed from "./pages/feed"
 import Contato from "./pages/Contato"
 import Login from "./pages/Login"
@@ -13,7 +12,6 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={ <Home />}></Route>
                 <Route path="/previsao" element={ <Previsao />}></Route>
-                <Route path="/buscaLocalizacao" element={ <BuscaLocalizacao />}></Route>
                 <Route path="/feed" element={ <Feed />}></Route>
                 <Route path="/contato" element={ <Contato />}></Route>
                 <Route path="/login" element={ <Login />}></Route>
