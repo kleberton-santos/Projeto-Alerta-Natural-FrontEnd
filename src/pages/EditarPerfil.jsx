ea719bf8d6e9fa631a7a52ac37698006c55077a1
Apolinario <whoami.apolo@gmail.com>
2025-01-08 17:38:58 -0300

    :sparkles: feat: add página de edição de perfil

--- src/pages/EditarPerfil.jsx
+++ src/pages/EditarPerfil.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 
-const EditarPerfil = () => {
-    return (
-        <div>
-            <h1>Editar Perfil</h1>
-        </div>
-    )
-}
+import { HeaderEditarPerfil } from "../components/header/HeaderEditarPerfil";
+import FooterGlobal from "../components/footer/FooterGlobal";
+import { SecaoEditarPerfil } from "../components/editarPerfil/SecaoEditarPerfil";
 
-export default EditarPerfil
+export const EditarPerfil = () => {
+  return (
+    <div className="home-principal d-flex flex-column min-vh-100">
+      {/* Header */}
+      <div className="header">
+        <HeaderEditarPerfil />
+      </div>
+
+      {/* NavBar */}
+      <div
+        className="nav-bar d-flex justify-content-center align-items-center border border-primary"
+        style={{ height: "55px" }}
+      >
+        <h2>NavBar</h2>
+      </div>
+
+      {/* Conteudo da Pagina*/}
+      <div className="flex-grow-1">
+        <SecaoEditarPerfil />
+      </div>
+
+      {/* Footer */}
+      <div>
+        <FooterGlobal />
+      </div>
+    </div>
+  );
+};
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,26 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Previsao from "./pages/Previsao"
-import Feed from "./pages/feed"
-import Contato from "./pages/Contato"
-import Login from "./pages/Login"
-import Cadastro from "./pages/Cadastro"
-import EditarPerfil from "./pages/EditarPerfil"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Previsao from "./pages/Previsao";
+import Feed from "./pages/feed";
+import Contato from "./pages/Contato";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import { EditarPerfil } from "./pages/EditarPerfil";
 
 const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <Home />}></Route>
-                <Route path="/previsao" element={ <Previsao />}></Route>
-                <Route path="/feed" element={ <Feed />}></Route>
-                <Route path="/contato" element={ <Contato />}></Route>
-                <Route path="/login" element={ <Login />}></Route>
-                <Route path="/cadastro" element={ <Cadastro />}></Route>
-                <Route path="/EditarPerfil" element={ <EditarPerfil />}></Route>
-
-            </Routes>
-        </BrowserRouter>
-    )
-}
-export default AppRoutes
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/previsao" element={<Previsao />}></Route>
+        <Route path="/feed" element={<Feed />}></Route>
+        <Route path="/contato" element={<Contato />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/cadastro" element={<Cadastro />}></Route>
+        <Route path="/EditarPerfil" element={<EditarPerfil />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default AppRoutes;
