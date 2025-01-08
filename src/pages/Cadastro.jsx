068928a223534fa4a6d8f05474b7800b35333c15
Apolinario <whoami.apolo@gmail.com>
2025-01-08 15:29:26 -0300

    :recycle: refactor: ajustes de layout e otimização do algoritmo

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -7,26 +7,26 @@ import { CadastroForm } from "../components/cadastro/CadastroForm";
 
 const Cadastro = () => {
     return (
-        <div className="home-principal ">
+        <div className="home-principal d-flex flex-column min-vh-100">
             {/* Header */}
             <div className="header">
                 <HeaderCadastro />
             </div>
 
             {/* NavBar */}
-            <div className="nav-bar d-flex justify-content-center align-items-center border border-primary" style={{ height: "55px", width: "100%", borderStyle: "solid" }}>
+            <div className="nav-bar d-flex justify-content-center align-items-center border border-primary" style={{ height: "55px"}}>
                 <h2>NavBar</h2>
             </div>
 
 
             {/* Conteudo da Pagina*/}
-            <div>
+            <div className="flex-grow-1">
                 <CadastroForm />
             </div>
 
 
             {/* Footer */}
-            <div className="footer mt-5" >
+            <div className="footer" >
                 <FooterGlobal />
             </div>
 
