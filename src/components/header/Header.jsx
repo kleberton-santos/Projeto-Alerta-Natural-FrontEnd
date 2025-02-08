601ead8e5b88b3847f74918f0b5fe7c0bdc733ea
kleberton-santos <ti.klebersantos@gmail.com>
2025-02-08 10:54:31 -0300

    inserido props para os header - ajustado headers e navbar em editar perfil

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import bannerHeader from "../../assets/images/banner-header.jpg";
+import logo from "../../assets/images/logo.webp";
+import "../../assets/Css/header/Header.css";
+import "../../assets/Css/header/logo.css";
+import PropTypes from "prop-types"; // Importando PropTypes
+
+const Header = ({ label }) => {
+  return (
+    <div className="header-principal position-relative">
+      <img src={bannerHeader} alt="Banner Header" className="header-banner img-fluid w-100" />
+      <div className="logo">
+        <img src={logo} alt="Logotipo alerta natural" className="logo-banner" />
+      </div>
+      <div className="label-principal position-absolute top-50 start-50 translate-middle text-white text-center fs-1 fw-bold">
+        <label htmlFor="">{label}</label>
+      </div>
+      <div className="login-principal position-absolute top-50 end-0 translate-middle-y d-flex align-items-center gap-2 me-3">
+        <Link to="/login" className="login-text text-light text-decoration-none fs-5">
+          Faça seu Login
+        </Link>
+        <div className="login-circle"></div>
+      </div>
+    </div>
+  );
+};
+
+// Definindo a validação para a prop 'label'
+Header.propTypes = {
+  label: PropTypes.string.isRequired, // 'label' deve ser uma string e é obrigatória
+};
+
+export default Header;
--- src/components/header/HeaderFeed.jsx
+++ src/components/header/HeaderFeed.jsx
@@ -1,29 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import bannerHeader from "../../assets/images/banner-header.jpg";
-import logo from "../../assets/images/logo.webp";
-import "../../assets/Css/header/Header.css"; 
-import "../../assets/Css/header/logo.css"; 
-
+import Header from "./Header";
 
 const HeaderFeed = () => {
-    return (
-        <div className="header-principal position-relative">
-            <img src={bannerHeader} alt="Banner Header" className="header-banner img-fluid w-100"/>
-                <div className="logo">
-                    <img src={logo} alt="Logotipo alerta natural" className="logo-banner" />
-                </div>
-                <div className="label-principal position-absolute top-50 start-50 translate-middle text-white text-center fs-1 fw-bold">
-                    <label htmlFor="">Feed</label>
-                </div>
-                <div className="login-principal position-absolute top-50 end-0 translate-middle-y d-flex align-items-center gap-2 me-3">
-                    <Link to="/login" className="login-text text-light text-decoration-none fs-5">
-                        Faça seu Login
-                    </Link>
-                <div className="login-circle"></div>
-            </div>
-        </div>
-    );
+  return <Header label="Feed" />;
 };
 
 export default HeaderFeed;
