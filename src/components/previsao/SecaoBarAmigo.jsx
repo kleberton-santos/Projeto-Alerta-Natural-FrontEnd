87ee20ed60d4a5139ffafc492d8116ae5abfdf85
kleberton-santos <ti.klebersantos@gmail.com>
2025-02-24 14:48:19 -0300

    ajuste na busca de amigos e estilos

--- src/components/previsao/SecaoBarAmigo.jsx
+++ src/components/previsao/SecaoBarAmigo.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
-import "../../assets/Css/previsao/SecaoBar.css";
+import "../../assets/Css/previsao/SecaoBarAmigo.css";
 
 const SecaoBarAmigo = ({ onSearch, isLoggedIn }) => {
   const [inputValue, setInputValue] = useState('');
 
+  // Atualiza o estado do input
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
+  // Realiza a busca ao clicar no botão ou pressionar Enter
   const handleSearch = () => {
     if (inputValue.trim() === "") {
       alert("Por favor, insira um nome para buscar.");
       return;
     }
     onSearch(inputValue); // Passa o valor da busca para o componente pai
+  };
+
+  // Função para lidar com o evento de pressionar uma tecla
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSearch(); // Chama a função de busca ao pressionar Enter
+    }
   };
 
   // Se o usuário não estiver logado, não renderiza o campo de busca
@@ -29,6 +38,7 @@ const SecaoBarAmigo = ({ onSearch, isLoggedIn }) => {
         className="secao-input"
         value={inputValue}
         onChange={handleInputChange}
+        onKeyDown={handleKeyDown} // Adiciona o evento de escuta para o teclado
       />
       <button className="secao-button" onClick={handleSearch}>
         <span role="img" aria-label="secao">🔍</span>
